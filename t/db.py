62b5214b8c47d5f918836a836dc9dd4158bb4858
Frankie <frankie@mint>
2013-06-29 21:16:43 -0400

    0.4.1

--- t/db.py
+++
@@ -1,9 +0,0 @@
-import landerdb
-
-sent = landerdb.Connect("sent.db")
-nodes = landerdb.Connect("nodes.db")
-data = landerdb.Connect("data.db")
-unsent = landerdb.Connect("unsent.db")
-messages = landerdb.Connect("messages.db")
-nonces = landerdb.Connect("nonces.db")
-addressdb = landerdb.Connect("addressbook.db")
